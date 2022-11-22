d7b600613c6dac08e96895334ee9d044e8b39d63
lucamabate <117281977+lucamabate@users.noreply.github.com>
2022-11-22 15:33:18 -0500

    Initial commit

---
+++ verilog/rtl/user_proj_example.v
@@ -0,0 +1,159 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj_example #(
+    parameter BITS = 32
+)(
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+     // IOs
+    input  [`MPRJ_IO_PADS-1:0] io_in,
+    output [`MPRJ_IO_PADS-1:0] io_out,
+    output [`MPRJ_IO_PADS-1:0] io_oeb
+
+);
+
+    wire clk;
+    wire rstn;
+    
+    wire [15:0] bcd;
+    wire [13:0] count;
+    wire [6:0] led;
+    
+    wire [`MPRJ_IO_PADS-1:0] io_in;
+    wire [`MPRJ_IO_PADS-1:0] io_out;
+    wire [`MPRJ_IO_PADS-1:0] io_oeb;
+
+    assign count[13:4] = 10'b0;
+
+    assign clk = io_in[10];
+    assign io_oeb[10] = 1'b1;
+    assign rstn = io_in[11];
+    assign io_oeb[11] = 1'b1;
+
+    assign io_out[18:12] = led[6:0];
+    assign io_oeb[18:12] = 7'b0;
+    
+    counter c0 (
+        .clk(clk),
+        .rstn(rstn),
+        .out(count[3:0])
+    );
+    
+    bin2bcd b0 (
+        .bin(count),
+        .bcd(bcd)
+    );
+    
+    segment7 s0 (
+        .bcd(bcd[3:0]),
+        .seg(led)
+    );
+  
+endmodule
+  
+module counter (
+        input   clk,
+        input   rstn,
+        output reg[3:0] out
+);
+
+always @ (posedge clk) begin
+  if (! rstn)
+        out <= 0;
+    else
+        out <= out + 1;
+end
+  
+endmodule
+
+
+module bin2bcd(
+   input [13:0] bin,
+   output reg [15:0] bcd
+   );
+   
+integer i;
+	
+always @(bin) begin
+    bcd=0;		 	
+    for (i=0;i<14;i=i+1) begin					//Iterate once for each bit in input number
+        if (bcd[3:0] >= 5) bcd[3:0] = bcd[3:0] + 3;		//If any BCD digit is >= 5, add three
+	if (bcd[7:4] >= 5) bcd[7:4] = bcd[7:4] + 3;
+	if (bcd[11:8] >= 5) bcd[11:8] = bcd[11:8] + 3;
+	if (bcd[15:12] >= 5) bcd[15:12] = bcd[15:12] + 3;
+	bcd = {bcd[14:0],bin[13-i]};				//Shift one bit, and shift in proper bit from input 
+    end
+end
+endmodule
+
+module segment7(
+     bcd,
+     seg
+    );
+     
+     //Declare inputs,outputs and internal variables.
+     input [3:0] bcd;
+     output [6:0] seg;
+     reg [6:0] seg;
+
+//always block for converting bcd digit into 7 segment format
+    always @(bcd)
+    begin
+        case (bcd) //case statement
+            0 : seg = 7'b0000001;
+            1 : seg = 7'b1001111;
+            2 : seg = 7'b0010010;
+            3 : seg = 7'b0000110;
+            4 : seg = 7'b1001100;
+            5 : seg = 7'b0100100;
+            6 : seg = 7'b0100000;
+            7 : seg = 7'b0001111;
+            8 : seg = 7'b0000000;
+            9 : seg = 7'b0000100;
+            //switch off 7 segment character when the bcd digit is not a decimal number.
+            default : seg = 7'b1111111; 
+        endcase
+    end
+    
+endmodule
+
+
+`default_nettype wire
